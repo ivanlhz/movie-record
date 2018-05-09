ea5a2cf0ba6834b313b114551b6b4b20ff2920a0
Iván López Hdez <ilopez@macbookpro.local>
2018-05-09 19:22:51 +0100

    Main styles and search header

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './style.css';
 
-const NotFound = () => <h1>404:Not found</h1>;
+const NotFound = () => (
+  <h1>
+    404 : <span>Not found</span>
+  </h1>
+);
 export default NotFound;
--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css';
+import * as movies from '../../data/movies.json';
 
-const Search = () => <h1>Search</h1>;
+class Search extends Component {
+  state = {
+    searchTerm: ''
+  };
+
+  handleSearchTerm = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    return (
+      <header>
+        <h1>Search</h1>
+        <input
+          onChange={this.handleSearchTerm}
+          value={this.state.searchTerm}
+          placeholder="type title to search..."
+          type="text"
+        />
+      </header>
+    );
+  }
+}
+
 export default Search;
