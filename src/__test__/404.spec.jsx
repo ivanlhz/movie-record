f7f6f7834da2f125dc174c8130f121bdf9b1910c
Iván López Hdez <ilopez@macbookpro.local>
2018-05-10 21:04:30 +0100

    Add Not found component tests

---
+++ src/__test__/404.spec.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import preload from '../../data/movies.json';
+
+import { NotFound } from '../components';
+
+configure({ adapter: new Adapter() });
+
+describe('NotFound', () => {
+  it('should be render', () => {
+    const component = shallow(<NotFound />);
+    expect(component).toMatchSnapshot();
+  });
+});
