6a585c07f797542ef93f3f3e30d1828d0495fe22
Iván López Hdez <ilopez@macbookpro.local>
2018-05-11 17:06:42 +0100

    Add test for the filter combo

--- src/__test__/Search.spec.jsx
+++ src/__test__/Search.spec.jsx
@@ -35,4 +35,52 @@ describe('Search', () => {
 
     expect(component.find(MovieCard).length).toEqual(0);
   });
+
+  it('should render correct amount of movies based on input search and filter year', () => {
+    const searchText = '2018';
+    const filter = 'year';
+    const component = shallow(<Search />);
+    const select = component.find('select');
+    const input = component.find('input');
+    input.simulate('change', { target: { value: searchText } });
+    select.simulate('change', { target: { value: filter } });
+
+    expect(component.find(MovieCard).length).toEqual(4);
+  });
+
+  it('should render 0 movies based on input search and filter year', () => {
+    const searchText = 'test';
+    const filter = 'year';
+    const component = shallow(<Search />);
+    const select = component.find('select');
+    const input = component.find('input');
+    input.simulate('change', { target: { value: searchText } });
+    select.simulate('change', { target: { value: filter } });
+
+    expect(component.find(MovieCard).length).toEqual(0);
+  });
+
+  it('should render correct amount of movies based on input search and filter genre', () => {
+    const searchText = 'horror';
+    const filter = 'genre';
+    const component = shallow(<Search />);
+    const select = component.find('select');
+    const input = component.find('input');
+    input.simulate('change', { target: { value: searchText } });
+    select.simulate('change', { target: { value: filter } });
+
+    expect(component.find(MovieCard).length).toEqual(1);
+  });
+
+  it('should render 0 movies based on input search and filter genre', () => {
+    const searchText = 'test';
+    const filter = 'genre';
+    const component = shallow(<Search />);
+    const select = component.find('select');
+    const input = component.find('input');
+    input.simulate('change', { target: { value: searchText } });
+    select.simulate('change', { target: { value: filter } });
+
+    expect(component.find(MovieCard).length).toEqual(0);
+  });
 });
